4ba59cf302b2c1976671276557b8f8559306ccb2
Htivus <suvithkumar0000@gmail.com>
2024-02-20 11:05:35 +0530

    containarisable , should train model with compatible versions

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,15 @@
-FROM python:3.8-alpine
+FROM python:3.6-slim
 
-# copy the requirements file into the image
-COPY ./requirements.txt /app/requirements.txt
+WORKDIR /app
+
+COPY . ./
 
 EXPOSE 5000
-# switch working directory
 
-RUN pip install numpy==1.21.5
-
-RUN pip install python-dev-tools
-
-RUN pip install pandas
-WORKDIR /app
 # install the dependencies and packages in the requirements file
-RUN pip install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+RUN python -m pip install --upgrade pip
 
-# copy every content from the local file to the image
-COPY . /app
 
-# configure the container to run in an executed manner
-ENTRYPOINT [ "python" ]
 
-CMD [ "python","app.py" ]
+CMD [ "python","api/index.py" ]
--- api/index.py
+++ api/index.py
@@ -4,53 +4,53 @@ from keras.preprocessing.sequence import pad_sequences
 from pickle import load
 import numpy as np
 
-app = Flask(__name__)
+app =Flask(__name__)
 
 # Load the ML model
-# model = load_model('model.h5')
+model = load_model('model.h5')
 
-# # Load the tokenizers
-# with open('kan_tokenizer.pkl', 'rb') as f:
-#     tokenizer_kan = load(f)
+# Load the tokenizers
+with open('kan_tokenizer.pkl', 'rb') as f:
+    tokenizer_kan = load(f)
 
-# with open('tulu_tokenizer.pkl', 'rb') as f:
-#     tokenizer_tulu = load(f)
+with open('tulu_tokenizer.pkl', 'rb') as f:
+    tokenizer_tulu = load(f)
 
 @app.route('/')
 def home():
     return "hi" 
 
-# render_template('index.html')
+render_template('index.html')
 
-# @app.route('/translate', methods=['POST','GET'])
-# def translate_text():
-#     try:
-#         input_text = request.json['text']  # Assuming JSON input
+@app.route('/translate', methods=['POST','GET'])
+def translate_text():
+    try:
+        input_text = request.json['text']  # Assuming JSON input
         
-#         print("Received input text:", input_text)  # Add this line for debugging
+        print("Received input text:", input_text)  # Add this line for debugging
         
-#         # Preprocess the input text
-#         input_sequence = tokenizer_kan.texts_to_sequences([input_text])
+        # Preprocess the input text
+        input_sequence = tokenizer_kan.texts_to_sequences([input_text])
         
-#         # Calculate the length of the input sequence dynamically
-#         # input_length = len(input_sequence[0])
+        # Calculate the length of the input sequence dynamically
+        # input_length = len(input_sequence[0])
         
-#         # Pad the input sequence based on its length
-#         input_sequence = pad_sequences(input_sequence, maxlen=24, padding='post')
+        # Pad the input sequence based on its length
+        input_sequence = pad_sequences(input_sequence, maxlen=24, padding='post')
         
-#         # Perform translation using the ML model
-#         translated_probabilities = model.predict(input_sequence)
+        # Perform translation using the ML model
+        translated_probabilities = model.predict(input_sequence)
         
-#         # Convert probabilities to letters
-#         translated_indices = [np.argmax(probabilities) for probabilities in translated_probabilities[0]]
-#         translated_text = ' '.join(tokenizer_tulu.index_word[index] for index in translated_indices if index != 0)
+        # Convert probabilities to letters
+        translated_indices = [np.argmax(probabilities) for probabilities in translated_probabilities[0]]
+        translated_text = ' '.join(tokenizer_tulu.index_word[index] for index in translated_indices if index != 0)
         
-#         return jsonify({'translated_text': translated_text}), 200
-#     except Exception as e:
-#         print("Error:", e)  # Add this line for debugging
-#         return jsonify({'error': str(e)}), 500
+        return jsonify({'translated_text': translated_text}), 200
+    except Exception as e:
+        print("Error:", e)  # Add this line for debugging
+        return jsonify({'error': str(e)}), 500
 
     
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000)
